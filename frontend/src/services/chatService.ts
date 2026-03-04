7a13cf943466496bc233f1bf3e8bfc35b87ccccd
harshit-technocrat11 <harshit2z1agrawala@gmail.com>
2026-03-04 20:08:41 +0530

    chat/ route needs to be connected to frontned

---
+++ frontend/src/services/chatService.ts
@@ -0,0 +1,15 @@
+export async function sendChat(question: string) {
+  const res = await fetch("http://localhost:8000/chat", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ question }),
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch response");
+  }
+
+  return res.json();
+}
