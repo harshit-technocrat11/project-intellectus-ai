bf0d1505f91259126e94a51c919fb3bfefd3acce
harshit-technocrat11 <harshit2z1agrawala@gmail.com>
2026-04-13 23:35:42 +0530

    goo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,13 @@
 import Dashboard from "./pages/Dashboard";
-import Copilot from "./pages/Copilot";
+import Copilot from "./pages/ChatCopilot";
 import { Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import AppLayout from "./app/layout/AppLayout";
-import Logs from "./pages/Logs";
+import Logs from "./pages/AILogs";
 
 function App() {
-  return (
-   <h1>hi</h1>
-  );
+  return <h1>hi</h1>;
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard.tsx";
-import Copilot from "./pages/Copilot.tsx";
-import Logs from "./pages/Logs.tsx";
+import Copilot from "./pages/ChatCopilot.tsx";
+import Logs from "./pages/AILogs.tsx";
 import AppLayout from "./app/layout/AppLayout.tsx";
 import "./index.css";
 
